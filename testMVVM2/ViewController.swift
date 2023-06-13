7bd03429970a8c2d4955f2a8f3cdc2828836641f
yuki-h29 <yuki.hirano29@gmail.com>
2023-06-13 20:55:53 +0900

    Initial Commit

---
+++ testMVVM2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testMVVM2
+//
+//  Created by 平野裕貴 on 2023/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
