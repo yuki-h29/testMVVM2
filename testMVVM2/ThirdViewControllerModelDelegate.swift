d9b0cea4b6951bb9d8bb1ddc6f4257968b392d63
yuki-h29 <yuki.hirano29@gmail.com>
2023-06-20 05:32:26 +0900

    製造；　非同期処理のコードサンプルを追加

--- testMVVM2/ThirdViewControllerModelDelegate.swift
+++ testMVVM2/ThirdViewControllerModelDelegate.swift
@@ -6,7 +6,15 @@
 //
 
 import Foundation
+import UIKit
+
 //ThirdViewControllerの宣言用
 protocol ThirdViewControllerModelDelegate {
+    //画面遷移を行う
+    func transitionTo(viewController: UIViewController)
     
+    //エラーメッセージを表示する
+    func showAlert(message: String)
+
 }
+
